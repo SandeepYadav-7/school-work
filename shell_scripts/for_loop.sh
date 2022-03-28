c5da92f9a0e797ea49036e78607b85e7565e53ae
SandeepYadav143 <sandeepyadav91577@gmail.com>
2022-03-28 19:47:21 +0530

    initial commit

---
+++ shell_scripts/for_loop.sh
@@ -0,0 +1,18 @@
+# For loop:
+
+# ===== syntax =====
+
+# for variable_name in value1, value2, value3...
+# do
+#     command 1
+#     command 2
+#     command 3
+# done
+
+# ==================
+
+clear
+for i in {1..10}
+do
+echo "We are inside for loop"
+done
---
+++ shell_scripts/while_loop.sh
@@ -0,0 +1,21 @@
+# While loop:
+
+# ===== syntax =====
+
+# while [ condition ]
+# do
+#     command 1
+#     command 2
+#     command 3
+# done
+
+# ==================
+
+clear
+a=1
+
+# -le means "less than or equal to"
+while [ $a -le 10 ]
+do
+echo "We are inside while loop $a"
+done
